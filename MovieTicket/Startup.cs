af7d53faeb5f40d56ef8b2818d338ef18f1a30c4
firaol81 <firaol.baneta@southeasttech.edu>
2024-05-02 07:57:41 -0500

    I try

--- MovieTicket/Startup.cs
+++ MovieTicket/Startup.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using MovieTicket.Models;
-using MovieTicket.Services;
+
 
 namespace MovieTicket
 {
